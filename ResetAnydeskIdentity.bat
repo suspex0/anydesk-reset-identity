f2a932a63bbaa766764aa699312fd1ca27916292
blank <64623405+suspex0@users.noreply.github.com>
2024-09-18 18:31:35 +0200

    Update ResetAnydeskIdentity.bat

--- ResetAnydeskIdentity.bat
+++ ResetAnydeskIdentity.bat
@@ -1,13 +1,1 @@
-@echo off
-set "programdata=%programdata%\AnyDesk"
-set "appdata=%appdata%\AnyDesk"
 
-echo Killing process..
-taskkill /f /im AnyDesk.exe
-
-echo Deleting files..
-del /q "%programdata%\*"
-del /q "%appdata%\*"
-
-echo Done.
-pause
